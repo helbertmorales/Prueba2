90fcd4dc3d7ccc43122e34fcd81bc0b15425726d
helbertmorales <helmorales@gmail.com>
2024-10-31 19:54:02 -0600

    Nuevo archivo

---
+++ Practica en clase.py
@@ -0,0 +1,9 @@
+numero= int(input("escriba el numero: "))
+resta= numero-100
+
+if numero>=100:
+    print("El valor es: " ,resta)
+elif numero<=100 and numero>0:
+    print("El valor es: " ,numero)
+else:
+    print("El numero es negativo")
